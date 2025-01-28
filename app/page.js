d7bbb5a34854479cb9e05e73106f8fc0fd7fe988
James McArthur <77163783+jamezmca@users.noreply.github.com>
2025-01-28 19:41:34 +1300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,20 +1,24 @@
+'use client'
 import ImageBanner from "@/components/ImageBanner";
 import Products from "@/components/Products";
+import { useEffect, useState } from "react";
 
 
 export async function getProducts() {
+  // what we'd do if we could deploy a proper backend
   const baseURL = process.env.NEXT_PUBLIC_BASE_URL
-  const response = await fetch(baseURL + 'api/products')
+  const response = await fetch(baseURL + '/api/products')
   const products = await response.json()
   return products
 }
 
-export default async function Home(props) {
-
-  const products = await getProducts()
+export default function Home(props) {
+  const [products, setProducts] = useState([])
+  // const products = await getProducts()
 
   let planner = null
   let stickers = []
+
 
   for (let product of products) {
     if (product.name === 'Medieval Dragon Month Planner') {
@@ -23,6 +27,22 @@ export default async function Home(props) {
     }
     stickers.push(product)
   }
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const baseURL = process.env.NEXT_PUBLIC_BASE_URL
+        const response = await fetch(baseURL + '/api/products')
+        const productsData = await response.json()
+        setProducts(productsData)
+      } catch (err) {
+        console.log(err.message)
+      }
+    }
+
+    fetchProducts()
+  }, [])
+
 
   return (
     < >
