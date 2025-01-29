184dc11be4289048d9c11cae025c1cd4ca41b298
James McArthur <77163783+jamezmca@users.noreply.github.com>
2025-01-29 16:15:28 +1300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,10 +2,16 @@ import ImageBanner from "@/components/ImageBanner";
 import Products from "@/components/Products";
 
 export async function getProducts() {
-    const baseURL = process.env.NEXT_PUBLIC_BASE_URL
-    const response = await fetch(baseURL + '/api/products')
-    const products = await response.json()
-    return products
+    try {
+
+        const baseURL = process.env.NEXT_PUBLIC_BASE_URL
+        const response = await fetch(baseURL + '/api/products')
+        const products = await response.json()
+        return products
+    } catch (err) {
+        console.log(err.message)
+        return []
+    }
 }
 
 export default async function Home(props) {
